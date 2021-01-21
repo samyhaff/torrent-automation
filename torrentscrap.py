f02fc791656b5769201627067670ef5bdda94558
samyhaff <haffoudhisamy@gmail.com>
2021-01-21 21:23:26 +0100

    retrieve all useful data

--- torrentscrap.py
+++ torrentscrap.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get("https://nyaa.si/?f=0&c=0_0&q=full+metal+alchemist+brotherhood&s=downloads&o=desc").text
+query = "full metal alchemist brotherhood".replace(" ", "+")
+source = requests.get("https://nyaa.si/?f=0&c=0_0&q=" + query +"&s=downloads&o=desc").text
 
 soup = BeautifulSoup(source, 'lxml')
 tbody = soup.find('tbody')
 tr = tbody.find('tr')
 for tr in tbody.find_all('tr'):
     tds = tr.find_all('td')
-    # print(tds[1].prettify())
     title = tds[1].find_all('a')[-1]
-    print(title.text)
+    link = tds[2].find('a')
+    size = tds[3].text
+    seeds = tds[5].text
