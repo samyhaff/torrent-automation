50824908160075e00edc8ce22fb2cf863ac863d0
samyhaff <haffoudhisamy@gmail.com>
2021-01-21 21:34:05 +0100

    python script should now be complete

--- torrentscrap.py
+++ torrentscrap.py
@@ -1,15 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
 
-query = "full metal alchemist brotherhood".replace(" ", "+")
+query = sys.argv[1].replace(" ", "+")
 source = requests.get("https://nyaa.si/?f=0&c=0_0&q=" + query +"&s=downloads&o=desc").text
 
 soup = BeautifulSoup(source, 'lxml')
 tbody = soup.find('tbody')
 tr = tbody.find('tr')
-for tr in tbody.find_all('tr'):
+for i, tr in enumerate(tbody.find_all('tr')):
     tds = tr.find_all('td')
-    title = tds[1].find_all('a')[-1]
-    link = tds[2].find('a')
+    title = tds[1].find_all('a')[-1].text
+    link = tds[2].find('a').get("href")
     size = tds[3].text
     seeds = tds[5].text
+    print(str(i) + " " + title + " " + size + " " + seeds + " " + link)
